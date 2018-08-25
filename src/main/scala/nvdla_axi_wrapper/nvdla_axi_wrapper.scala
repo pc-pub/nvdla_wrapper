4b9eef612ab9869caa2c3c3fff3809a591a76214
Dai, Pengcheng <dpc_work@163.com>
2018-08-25 23:00:51 +0800

    Change AXI port name.

--- src/main/scala/nvdla_axi_wrapper/nvdla_axi_wrapper.scala
+++ src/main/scala/nvdla_axi_wrapper/nvdla_axi_wrapper.scala
@@ -9,15 +9,15 @@ import axi2dbb._
 
 class NvdlaAXIWrapper extends RawModule {
   val core_port = IO(new NvdlaCorePort)
-  val s00_axi = IO(Flipped(new AXILitePort(18, 1, "s00_axi_")))
-  val m03_axi = IO(new AXIFullPort(32, 2, 8, 0, "m03_axi"))
+  val S00_axi = IO(Flipped(new AXILitePort(18, 1, "S00_axi_")))
+  val M00_axi = IO(new AXIFullPort(32, 2, 8, 0, "M00_axi"))
 
   val nvdla = Module(new NV_nvdla())
   val axi2csb = withClockAndReset(core_port.dla_csb_clock, !core_port.dla_reset_rstn)(Module(new AXI2CSB()))
   val axi2dbb = withClockAndReset(core_port.dla_core_clock, !core_port.dla_reset_rstn)(Module(new AXI2DBB()))
   core_port <> nvdla.io.core_port_replaced
-  s00_axi <> axi2csb.io.axi
-  m03_axi <> axi2dbb.io.axi
+  S00_axi <> axi2csb.io.axi
+  M00_axi <> axi2dbb.io.axi
   axi2csb.io.csb <> nvdla.io.csb_port_replaced
   axi2dbb.io.dbbif <> nvdla.io.nvdla_core2dbb
 }
